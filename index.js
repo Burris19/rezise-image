20abe8ed5c488f423567e49314c8dddfca84887e
Burris19 <julianhernandez19@gmail.com>
2017-01-31 23:29:30 -0600

    [add] return objet with url images

--- index.js
+++ index.js
@@ -4,85 +4,90 @@ var Kraken = require('kraken')
 var fs = require('fs')
 var async = require('async')
 var request = require('request')
+var Promise = require("bluebird");
 var configTypes = require('./config.json')
 
 var externals = {}
+var kraken = new Kraken({
+	'api_key': '3b566aa6c8c21d78f2ac628cdeb7b84e',
+	'api_secret': '099f09d9395a3e9a03c131ac0ef1fa067f996a20'
+})	
+var urlObject = []
+var objectParameter = []
 
-externals.sendRequest = function (urlService) {
-	return new Promise(function (resolve, reject) {
-		console.log('Url callback', urlService)
-		request(urlService, function (error, response, body) {
-			resolve('ok')
-		})
+externals.sendRequest = function (urlService, id, data) {
+	return new Promise(function (resolve, reject) {	
+		request.post({
+        	url: urlService,
+         	body: {
+         		'id': id,
+         		'data': data
+         	}
+        }, function(error, response, body){
+            resolve('ok')
+    	});
+
 	})
 }
 
-externals.callToNext = function (success, message, callback) {
-    if (success) {
-        return callback()
-    }
-    
-    return callback(message)
+externals.uploadImages = function(settings){
+	return 	new Promise(function(resolve, reject){
+		async.each(settings.configTypes, function (file, callback) {
+			var params = {
+				url: settings.urlImage,			
+				wait: true,
+				resize: {
+					width: parseInt(file.width),
+					height: parseInt(file.height),
+					strategy: 'exact'
+				},
+				s3_store: {
+					key: 'AKIAJBVITO43KTLBDOLQ',
+					secret: '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',
+					bucket: 'test-lambda-smartly',
+					path: 'images/' + settings.typeParameter + '/' + settings.idParameter + file.width + 'x' + file.height + '.jpg'
+				},
+				webp: true,
+				lossy: true
+			}
+
+			kraken.url(params, function (response) {
+				if (response.success) {
+	        		settings.urlObject.push({'url': response.kraked_url, 'success': response.success});		        		
+	    		}
+	    		callback()
+			})		
+
+		}, function (err) {
+			resolve(settings.urlObject)
+		})	
+	})
 }
 
-externals.handler = function(event, context, callback) {
-
-	var kraken = new Kraken({
-		'api_key': '3b566aa6c8c21d78f2ac628cdeb7b84e',
-		'api_secret': '099f09d9395a3e9a03c131ac0ef1fa067f996a20'
-	})	
-
+externals.handler = function(event, context) {
+	
 	var idParameter = event.query.id === undefined ? Math.floor((Math.random() * 10000)) : event.query.id	
 	var urlCallback = event.query.callback
 	var typeParameter = event.query.type
 
 	if (!typeParameter in configTypes)
 		typeParameter = 'facebook'
+	
+	objectParameter['configTypes'] = configTypes[typeParameter]
+	objectParameter['urlImage'] = event.query.url
+	objectParameter['typeParameter'] = typeParameter
+	objectParameter['idParameter'] = idParameter
+	objectParameter['urlObject'] = urlObject
 
-	async.each(configTypes[typeParameter], function (file, callback) {
-		var params = {
-			url: event.query.url,			
-			wait: true,
-			resize: {
-				width: parseInt(file.width),
-				height: parseInt(file.height),
-				strategy: 'exact'
-			},
-			s3_store: {
-				key: 'AKIAJBVITO43KTLBDOLQ',
-				secret: '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',
-				bucket: 'test-lambda-smartly',
-				path: 'images/' + typeParameter + '/' + idParameter + file.width + 'x' + file.height + '.jpg'
-			},
-			webp: true,
-			lossy: true
-		}
-
-		kraken.url(params, function (response) {			
-
+	externals.uploadImages(objectParameter)
+		.then(function(data){			
 			if (urlCallback) {
-				var success = (response.success) ? 'true' : 'false'
-				var urlCallbackResponse = urlCallback + '?id=' + idParameter + '&success=' + success
-			    externals.sendRequest(urlCallbackResponse)
-				    .then(() => {
-				        externals.callToNext(response.success, response.message, callback)
-				    })
-			} else {
-			    externals.callToNext(response.success, response.message, callback)
+				return externals.sendRequest(urlCallback, idParameter, JSON.stringify(data))
 			}
-			
+			return null 
+		}).then(function(response){
+			context.done()
 		})
-
-	}, function (err) {
-		if (err) {
-			console.log('A file failed to process')
-			callback(err, {"message": "Processing request id " + idParameter})
-		} else {
-			console.log('A file success to process')
-			callback(null, {"message": "Processing request id " + idParameter})
-		}
-	})	
 }
-
 
 module.exports = externals
